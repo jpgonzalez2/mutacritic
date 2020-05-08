b5a5b0a720bcb392aa13a7236d78b403fed7a92c
jpgonzalez2 <jorgebolt98@gmail.com>
2020-05-08 14:14:49 +0200

    lista de comentarios

---
+++ handlers/comentarios/lista.py
@@ -0,0 +1,28 @@
+# Lista de comentarios sobre un videojuego
+
+
+import webapp2
+from webapp2_extras import jinja2
+
+
+from model.videojuego import Videojuego
+from model.comentario import Comentario
+
+class ListaComentariosHandler(webapp2.RequestHandler):
+    def get(self):
+        comentarios = Comentario.recupera_para(self.request)
+
+        valores_plantilla = {
+            "comentarios": comentarios
+        }
+
+        jinja = jinja2.get_jinja2(app=self.app)
+        self.response.write(
+            jinja.render_template("lista_comentarios.html",
+            **valores_plantilla))
+
+
+
+app = webapp2.WSGIApplication([
+    ('/comentarios/lista', ListaComentariosHandler)
+], debug=True)
--- model/comentario.py
+++ model/comentario.py
@@ -4,9 +4,26 @@
 
 from google.appengine.ext import ndb
 
+from videojuego import Videojuego
+
 
 class Comentario(ndb.Model):
     usuario = ndb.StringProperty(required=True)
     hora = ndb.DateTimeProperty(auto_now_add=True, indexed=True)
     puntuacion = ndb.IntegerProperty(required=True)
     texto = ndb.StringProperty(required=True)
+    clave_videojuego = ndb.KeyProperty(kind=Videojuego)
+
+    @staticmethod
+    def recupera_para(req):
+        try:
+            id_vdj = req.GET["vdj"]
+        except KeyError:
+            id_vdj = ""
+
+        if id_vdj:
+            clave_videojuego = ndb.Key(urlsafe=id_vdj)
+            comentarios = Comentario.query(Comentario.clave_videojuego == clave_videojuego)
+            return comentarios
+        else:
+            print("ERROR: videojuego no encontrado")
