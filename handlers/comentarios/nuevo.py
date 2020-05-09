7828c8a8ebd8485f3e0e46429fd052f35d323385
jpgonzalez2 <jorgebolt98@gmail.com>
2020-05-09 16:24:38 +0200

    sumatorio de las puntuaciones

--- handlers/comentarios/nuevo.py
+++ handlers/comentarios/nuevo.py
@@ -8,6 +8,7 @@ from webapp2_extras import jinja2
 from google.appengine.ext import ndb
 
 from model.comentario import Comentario
+from model.videojuego import Videojuego
 
 
 class NuevoComentarioHandler(webapp2.RequestHandler):
@@ -41,6 +42,18 @@ class NuevoComentarioHandler(webapp2.RequestHandler):
                                     clave_videojuego=ndb.Key(urlsafe=clave_videojuego))
             comentario.put()
             time.sleep(1)
+
+            suma = 0
+            cont = 0
+            videojuego, comentarios = Comentario.recupera_para(self.request)
+            for comentario in comentarios:
+                suma += int(comentario.puntuacion)
+                cont += 1
+            res = suma / cont
+            videojuego.puntuacion = res
+            videojuego.put()
+            time.sleep(1)
+
             return self.redirect("/comentarios/lista?vdj=" + clave_videojuego)
 
 
