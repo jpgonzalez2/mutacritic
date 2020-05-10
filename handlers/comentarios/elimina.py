08430e470175c08352df0333d1744cec3fcff293
jpgonzalez2 <jorgebolt98@gmail.com>
2020-05-10 12:33:51 +0200

    añadido sumatorio puntuaciones

--- handlers/comentarios/elimina.py
+++ handlers/comentarios/elimina.py
@@ -15,6 +15,18 @@ class EliminaComentarioHandler(webapp2.RequestHandler):
         comentario = Comentario.recupera(self.request)
         comentario.key.delete()
         time.sleep(1)
+
+        suma = 0
+        cont = 0
+        videojuego, comentarios = Comentario.recupera_para(self.request)
+        for comentario in comentarios:
+            suma += int(comentario.puntuacion)
+            cont += 1
+        res = suma / cont
+        videojuego.puntuacion = res
+        videojuego.put()
+        time.sleep(1)
+
         return self.redirect("/comentarios/lista?vdj=" + clave_videojuego)
 
 app = webapp2.WSGIApplication([
